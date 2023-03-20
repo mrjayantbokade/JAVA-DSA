9805e1b5a70c414b68bb5a2c35a9f614c80deac6
Jayant Bokade <112440030+mrjayantbokade@users.noreply.github.com>
2023-03-20 19:50:28 +0530

    prime

--- arrays/src/Multidimensional_ArrayList.java
+++ arrays/src/Multidimensional_ArrayList.java
@@ -35,7 +35,7 @@ public class Multidimensional_ArrayList {
 //        }
 
 
-        
+
 //        for(int i=0; i<3; i++){
 //            for (int j=0; j<3; j++){
 //                System.out.print(list.get(i).get(j)+" ");
---
+++ arrays/src/Swap.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+
+
+
+
+
+        int[] arr = {1,2,3,4,5};
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+
+        swap( arr,0,4);
+    }
+
+    /*This function is built for swapping the elements in the array on the basis of the
+   index provided by the user in the function*/
+
+    public static void swap(int[] arr, int index1, int index2){
+
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+
+
+//       int temp = arr[a];
+//        arr[a] = arr[b];
+//        arr[b] = temp;
+
+
+       for (int num: arr){
+            System.out.print(num+" ");
+        }
+
+
+
+
+
+   }
+}
