69166515632bd6e862a7514bdc127e4aa44b7d08
Jayant Bokade <112440030+mrjayantbokade@users.noreply.github.com>
2023-03-23 09:25:17 +0530

    find min in arr using linear search

---
+++ Searcing_Algorithms/src/Find_min_num.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Find_min_num {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 24, 53, 532,45,-4,-1, -5,-9};
+
+        System.out.println(min(arr));
+
+    }
+
+
+    //this program will return the min num present in the array using linear search
+    public static int min(int[] arr){
+
+        int num_min = arr[0];
+        for (int i=1; i< arr.length; i++){
+            if(num_min > arr[i]){
+                num_min = arr[i];
+            }
+
+        }
+        return num_min;
+    }
+
+}
