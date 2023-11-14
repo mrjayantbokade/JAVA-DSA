033c46e46e6692a4125ce80945f30dcf1f67d556
mrjayantbokade <mrjayant81@gmail.com>
2023-11-14 09:10:34 +0530

    math_for_dsa

---
+++ Math_for_DSA/RightShiftoperator.java
@@ -0,0 +1,8 @@
+public class RightShiftoperator {
+
+
+    public static void main(String[] args) {
+        int a  = 10;
+        System.out.println(a << 1); //Anything leftshift with 1 will double the number
+    }
+}
