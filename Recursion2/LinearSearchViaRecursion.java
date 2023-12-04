f3bce6026a6ec2427abe5379d4e615c786da45a8
mrjayantbokade <mrjayant81@gmail.com>
2023-12-04 22:58:36 +0530

    c

--- Recursion2/LinearSearchViaRecursion.java
+++ Recursion2/LinearSearchViaRecursion.java
@@ -1,4 +1,14 @@
 public class LinearSearchViaRecursion {
+
+
+
+    public static boolean kunalsapproach(int[] arr, int target, int index){
+        if (index == arr.length){
+            return false;
+        }
+
+        return arr[index] == target  || kunalsapproach(arr, target, index+1);
+    }
     public static int linearSearch(int [] arr, int target, int index){
 
         if (index == arr.length-1){
@@ -18,7 +28,8 @@ public class LinearSearchViaRecursion {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 2, 4, 2, 33};
-        int target =88;
+        int target =33;
         System.out.println(linearSearch(arr, target, 0));
+        System.out.println(kunalsapproach(arr, target, 0));
     }
 }
