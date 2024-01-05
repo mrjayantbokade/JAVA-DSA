ca7ee0d79c88bbfec11c177f7bc0db9b9e9023d5
jayant bokade <mrjayant81@gmail.com>
2024-01-05 13:45:22 +0530

    linked list custom

--- LinkedList/src/jayant/LL.java
+++ LinkedList/src/jayant/LL.java
@@ -1,4 +1,5 @@
 package jayant;
+import java.util.*;
 
 public class LL {
 
@@ -9,6 +10,63 @@ public class LL {
         this.size = 0;
     }
 
+
+
+    public void deleteLast(){
+
+        Node temp = head;
+        for (int i = 1; i < size-1; i++) {
+
+            temp = temp.next;
+            System.out.println(temp.value);
+
+        }
+        temp.next = null;
+        size--;
+
+    }
+    public void deleteFirst(){
+
+        Node temp = head.next;
+        head = temp;
+        size--;
+
+    }
+    public void insertAtIndex  (int value, int index)  {
+
+
+        if (index > size+1){
+            System.out.println("Cannot place at this index");
+            System.out.println("Because it is not of that length");
+        }
+
+
+        if (index == size){
+            add(value);
+        }
+
+        if (index == 1){
+            insertFirst(value);
+        }else {
+            Node newnode  = new Node(value);
+            Node temp = head;
+            for (int i = 2; i < index ; i++) {
+                temp = temp.next;
+            }
+            Node currentNode = temp.next;
+            temp.next = newnode;
+            newnode.next = currentNode;
+            size++;
+            System.out.println("node inserted");
+        }
+
+
+
+
+
+
+
+    }
 
     public void display(){
         Node temp = head; //temp node
@@ -21,26 +79,36 @@ public class LL {
         }
 
         System.out.print(tail.next);
+        System.out.println();
 
 
     }
     public void add(int value){
         Node newnode = new Node(value);
-//        if (head == null){
-//            head = newnode;
-//            tail = head;
+        if (tail == null){
+            insertFirst(value);
+        }else {
+
+            tail.next = newnode;
+            tail = newnode;
+            size++;
+            System.out.println("node inserted ");
+
+            //this approach is being helpful when there is no extra object called tail and this needs
+            //O(n) time complexity also
+//            Node temp = head;
+//            while (temp.next != null){
+//                temp = temp.next;
+//
+//            }
+//            temp.next = newnode;
+//            tail = newnode;
+////        newnode.next = null;
 //            size += 1;
-//        }
-
-        Node temp = head;
-        while (temp.next != null){
-            temp = temp.next;
-
         }
-        temp.next = newnode;
-        tail = newnode;
-//        newnode.next = null;
-        size += 1;
+
+
+
     }
     public  void insertFirst(int value){
 
@@ -52,7 +120,7 @@ public class LL {
         }
 
         size += 1;
-        System.out.println("node inserted");
+        System.out.println("node inserted ");
 
 
     }
--- LinkedList/src/jayant/Main.java
+++ LinkedList/src/jayant/Main.java
@@ -9,10 +9,25 @@ public class Main {
         LL list = new LL();
 
         list.insertFirst(44);
-        list.insertFirst(44);
+        list.insertFirst(84);
         list.add(66);
+        list.add(646);
+        list.insertAtIndex(55, 1);
+        list.insertAtIndex(22, 5);
+        list.insertAtIndex(3, 6);
+        list.insertAtIndex(123, 7);
         System.out.println(list.size);
         list.display();
+        System.out.println();
+        list.deleteFirst();
+        list.display();
+        System.out.println();
+        System.out.println(list.size);
+        list.deleteLast();
+        System.out.println(list.size);
+        list.display();
+
+
 
 
     }
